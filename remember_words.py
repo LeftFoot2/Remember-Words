6b7c66330410d50a37d963e4e75330de7d70d149
Thomas Jamieson <jam18007@byui.edu>
2024-02-02 22:19:15 -0700

    Added database, changed adding window to have buttons.

--- remember_words.py
+++ remember_words.py
@@ -5,6 +5,20 @@ from PyQt5 import QtWidgets
 from remember_words_gui_template_1 import Ui_MainWindow
 from add_word_window_1 import Ui_add_word_window
 import sys
+import sqlite3
+
+
+
+
+conn = sqlite3.connect('word_bank.db')
+
+cur = conn.cursor()
+
+cur.execute("""CREATE TABLE if not exists words_list(word text)""")
+
+conn.commit()
+
+conn.close()
 
 
 class MainWindow(QMainWindow):
@@ -17,7 +31,7 @@ class MainWindow(QMainWindow):
 
 
         self.alphabet = ["a__________","b__________","c__________","d__________","e__________","f__________","g__________","h__________","i__________","j__________","k__________","l__________","m__________","n__________","o__________","p__________","q__________","r__________","s__________","t__________","u__________","v__________","w__________","x__________","y__________","z__________"]
-        self.word_list = []
+        # self.word_list = []
 
 
         # Buttons clicked
@@ -41,7 +55,43 @@ class MainWindow(QMainWindow):
         self.add_ui.setupUi(self.add_win)
         self.add_win.show()
 
-        self.add_ui.add_word_line.returnPressed.connect(self.enter_pressed_add)
+        self.add_ui.add_word_line.returnPressed.connect(self.add_window_button)
+        self.add_ui.adding_pushButton.clicked.connect(self.add_window_button)
+        self.add_ui.cancel_add_pushButton.clicked.connect(self.close_window_button)
+
+
+    
+    def close_window_button(self):
+        self.add_win.close()
+
+
+    def add_window_button(self):
+        ##### TODO ####
+        # Add a way to varify that it is a valid word
+        # aka no numbers or speshel charactors
+
+
+        conn = sqlite3.connect('word_bank.db')
+
+        cur = conn.cursor()
+
+        cur.execute("INSERT INTO words_list VALUES (:word)",
+        {
+            'word': self.add_ui.add_word_line.text()
+        }
+        )
+        self.ui.word_bank.addItem(self.add_ui.add_word_line.text())
+
+        
+        # self.word_list.append(self.add_ui.add_word_line.text()) # Pointless now. This will have to be a something that can be altered SQL?
+
+        conn.commit()
+
+        conn.close()
+
+
+        self.ui.word_bank.sortItems()
+        self.add_win.close()
     
     # Called if the 'remove' button was pressed
     def remove_word(self):
@@ -59,7 +109,7 @@ class MainWindow(QMainWindow):
             self.delete_warning.setText(f'Are you sure you want to delete: "{current_word.text()}"')
             self.delete_warning.setIcon(QMessageBox.Warning)
             self.delete_warning.setStandardButtons(QMessageBox.Yes|QMessageBox.No)
-            self.delete_warning.buttonClicked.connect(self.pop_buttons)
+            self.delete_warning.buttonClicked.connect(self.option_buttons)
 
             self.delete_warning.exec_()
         
@@ -68,13 +118,30 @@ class MainWindow(QMainWindow):
 
 
 
-    def pop_buttons(self, answer):
+    def option_buttons(self, answer):
         
         if answer.text() == '&Yes':
-            
+
             current_row = self.ui.word_bank.currentRow()
+            current_word = self.ui.word_bank.currentItem()
 
             self.ui.word_bank.takeItem(current_row)
+
+            conn = sqlite3.connect('word_bank.db')
+
+            cur = conn.cursor()
+
+            delete_query = """DELETE FROM words_list WHERE word = ?"""
+
+            cur.execute(delete_query, (current_word.text(),))
+
+            conn.commit()
+
+            conn.close()
+            
+            # current_row = self.ui.word_bank.currentRow()
+
+            # self.ui.word_bank.takeItem(current_row)
 
         elif answer.text() == '&No':
             pass
@@ -85,18 +152,33 @@ class MainWindow(QMainWindow):
 
 
         
-    def enter_pressed_add(self):
-        self.word_list.append(self.add_ui.add_word_line.text()) # Pointless now. This will have to be a something that can be altered SQL?
-        self.ui.word_bank.addItem(self.add_ui.add_word_line.text())
-        self.ui.word_bank.sortItems()
-        self.add_win.close()
+        
 
 
     def load_words(self):
+
+
+
+        conn = sqlite3.connect('word_bank.db')
+
+        cur = conn.cursor()
+
+        cur.execute("SELECT * FROM words_list")
+        word_record = cur.fetchall()
+        
+        conn.commit()
+
+        conn.close()
         # self.ui.word_bank.clear()
+
         self.ui.word_bank.addItems(self.alphabet)
-        self.ui.word_bank.addItems(self.word_list)
+
+        for record in word_record:
+            self.ui.word_bank.addItems(record)
+
+
         self.ui.word_bank.sortItems()
+        
 
 
 
